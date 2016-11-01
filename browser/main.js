2ac85a800ef308fd2316038b6361192976ea14df
Jake Peyser <jakepeyser@gmail.com>
2016-11-01 17:17:16 -0400

    finished front end boilerplate

--- browser/main.js
+++ browser/main.js
@@ -1,17 +1,23 @@
-'use strict'
+// Libraries
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux'
 
-import store from './store'
+// React components
 import App from './components/App'
 import AllProductsContainer from './components/products/AllProductsContainer'
+
+// Redux actions and thunks
+import store from './store'
+import { fetchProducts } from './redux/products'
+
+const appEnter = () => store.dispatch(fetchProducts());
 
 render(
   <Provider store={ store }>
     <Router history={ browserHistory }>
-      <Route path="/" component={ App }>
+      <Route path="/" component={ App } onEnter={ appEnter }>
         <Route path="/products" component={ AllProductsContainer } />
         <IndexRoute component={ AllProductsContainer } />
       </Route>
