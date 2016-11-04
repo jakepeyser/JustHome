226811fdecfab2cb4a8bae2c282d1dc5b5aca882
rjisoo <jisoo.cc@gmail.com>
2016-11-04 13:54:20 -0400

    cart routes #24 #25 #26 #27 #31

--- db/models/cartProduct.js
+++ db/models/cartProduct.js
@@ -6,7 +6,35 @@ const db = require('APP/db')
 const cartProduct = db.define('cartProducts', {
 	sessionId: Sequelize.STRING,
 	quantity: Sequelize.INTEGER
+}, {
+	validate: {
+		noDuplicatedCartProducts: function(next) {
+			cartProduct.findOne({
+				where: {
+					sessionId: this.sessionId,
+					product_id: this.product_id
+				}
+			})
+			.then(result => {
+				if (result !== null) {
+					next('Product already in the cart')
+				}
+				next()
+			})
+		}
+	},
+	classMethods: {
+		clearCart: function(sessionId) {
+			console.log('sessionId: ', sessionId)
+			return 	cartProduct.destroy({
+					where: { sessionId: sessionId }
+				})
+		}
+	}
 })
+
+// TODO: session + product should be unique.
+
 
 // Authed user
 // // create asso with user
@@ -17,3 +45,4 @@ const cartProduct = db.define('cartProducts', {
 // // local storage (put object and save it as cookie)
 
 module.exports = cartProduct;
+
